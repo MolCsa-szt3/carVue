c4a764bc9d1b4d9ce399982a46db0022051f905f
szt3molcsa <szt3molcsa@vasvari.org>
2025-01-29 13:18:53 +0100

    smol progress

--- src/views/AddView.vue
+++ src/views/AddView.vue
@@ -1,5 +1,32 @@
 <template>
   <h2>Add a car!</h2>
+
+  <form @submit.prevent="save">
+    <input type="text" v-model="brand" placeholder="Brand"><br>
+    <input type="text" v-model="model" placeholder="Model"><br>
+    <input type="number" v-model="year"><br>
+    <button>Mentés</button>
+  </form>
 </template>
 
-<style></style>
+<style>
+</style>
+<script setup>
+import axios from "axios";
+
+import { onMounted, ref } from "vue";
+
+const apiClient = axios.create({
+  baseURL: "https://surveys-5jvt.onrender.com/api/cars/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+const model = ref()
+const brand = ref()
+const year = ref(2000)
+const save = ()=>{
+
+}
+
+</script>
